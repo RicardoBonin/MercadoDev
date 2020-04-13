5a403cf6a4a85b2e802297fbad86061b21265e2a
Ricardo <ricardo@brittes.com.br>
2020-04-12 23:08:24 -0300

    Conf Route

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
 import React from 'react'
-import HeaderHome from './HeaderHome'
-import AnuncioHome from './AnuncioHome'
+import Home from './Home'
 import Footer from './Footer'
-import LinkCategoria from './LinkCategoria'
-import {  useDatabase } from './database'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
-  const dataCategorias = useDatabase('categorias')
-  const dataAnuncios = useDatabase('anuncios')
-  const idsCat = Object.keys(dataCategorias)
-  const idsAnun = Object.keys(dataAnuncios)
-  let index = 0
   return (
-    <div className="App">
-      <HeaderHome />
-      <div className='container'>
-        <h3>Útimos Anúncios</h3>
-        <div className='row'>
-          { idsAnun.map( cat => {
-            return <AnuncioHome key={cat} anuncio={dataAnuncios[cat]}/>
-          })
-          }
-        </div>
-        <h3>Categorias</h3>
-        <div className='row'>
-          { idsCat.map( cat => {
-            return  [
-                     <LinkCategoria key={cat} categoria={dataCategorias[cat]} />,
-                     ++index%4 === 0 && <div key={'c'+cat} className='w-100'></div>
-                    ]
-          })
-          }
-        </div>
+    <Router>
+      <div className="App">
+        <Route path='/' exact component={Home} />
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
+    
   )
 }
 
