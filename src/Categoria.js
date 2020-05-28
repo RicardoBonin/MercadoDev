e4852eb12c105cc63efc1a44b285908c9e01a52a
Ricardo Bonin <ricardo@brittes.com.br>
2020-05-27 23:14:05 -0300

    Finalizando o Componente Categoria!

--- src/Categoria.js
+++ src/Categoria.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import AnuncioHome from "./AnuncioHome";
+
+const url =
+  "https://mercadodev-c70f7.firebaseio.com/anuncios.json?orderBy=%22categoria%22&equalTo=%22animais-e-acessorios%22";
 
 const Categoria = (props) => {
+  const [state, setState] = useState({ anuncios: {} });
+  const anuncios = props.match.params.id;
+  const url = `https://mercadodev-c70f7.firebaseio.com/anuncios.json?orderBy=%22categoria%22&equalTo=%22${anuncios}%22`;
+
+  useEffect(() => {
+    axios.get(url).then((res) => setState({ anuncios: res.data }));
+  }, [url]);
+
   console.log(props);
-  return <h1>Categoria: {JSON.stringify(props.match.params.id)}</h1>;
+  return (
+    <div>
+      <h1>Categoria: {JSON.stringify(anuncios)}</h1>;
+      <div className="row">
+        {Object.keys(state.anuncios).map((anuncio) => {
+          return (
+            <AnuncioHome key={anuncio} anuncio={state.anuncios[anuncio]} />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Categoria;
