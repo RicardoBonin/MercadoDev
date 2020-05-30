5d8f53b3983caa53121d298d307344a97a0c63da
Ricardo Bonin <ricardo@brittes.com.br>
2020-05-30 00:06:14 -0300

    Finalizando o Projeto

--- src/Anuncio.js
+++ src/Anuncio.js
@@ -3,14 +3,18 @@ import axios from "axios";
 
 const Anuncio = (props) => {
   console.log(props.props.match.params.idAnuncio);
-  const [state, setState] = useState({ anuncio: {} });
+  const [state, setState] = useState({ anuncio: {}, isLoding: true });
   const id = props.props.match.params.idAnuncio;
   const url = `https://mercadodev-c70f7.firebaseio.com/anuncios/${id}.json`;
   useEffect(() => {
     axios.get(url).then((data) => {
-      setState({ anuncio: data.data });
+      setState({ ...state, anuncio: data.data, isLoding: false });
     }, []);
   });
+
+  if (state.isLoding) {
+    return <i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>;
+  }
 
   return (
     <div>
--- src/Categoria.js
+++ src/Categoria.js
@@ -3,17 +3,28 @@ import axios from "axios";
 import AnuncioHome from "./AnuncioHome";
 
 const Categoria = (props) => {
-  const [state, setState] = useState({ anuncios: {} });
+  const [state, setState] = useState({ anuncios: {}, isLoading: false });
   const anuncios = props.match.params.id;
   const url = `https://mercadodev-c70f7.firebaseio.com/anuncios.json?orderBy=%22categoria%22&equalTo=%22${anuncios}%22`;
 
+  const carregar = async () => {
+    setState({ ...state, isLoading: true, anuncios: {} });
+    const res = await axios.get(url);
+    setState({ anuncios: res.data, isLoading: false });
+  };
   useEffect(() => {
-    axios.get(url).then((res) => setState({ anuncios: res.data }));
+    carregar();
   }, [url]);
 
   return (
     <div>
-      <h1>Categoria: {JSON.stringify(anuncios)}</h1>
+      <h1>Categoria: {anuncios}</h1>
+      {state.isLoading && (
+        <i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
+      )}
+      {!state.isLoading && Object.keys(state.anuncios).length === 0 && (
+        <p>Nenhum produto cadastrado!</p>
+      )}
       <div className="row">
         {Object.keys(state.anuncios).map((anuncio) => {
           return (
